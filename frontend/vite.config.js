006eee2f967f7afff3c976773d24359c21090b4a
META-Xiao <makeyxiao@gmail.com>
2026-02-23 23:40:49 +0800

    Optimize loading time

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,11 +14,27 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: false,
     minify: 'esbuild',
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
-          'vue-vendor': ['vue', 'vue-router']
-        }
+          'vue-vendor': ['vue', 'vue-router'],
+          'echarts': ['echarts'],
+          'anime': ['animejs'],
+          'iconify': ['@iconify/vue']
+        },
+        // 优化文件名
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    },
+    // 压缩选项
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
       }
     }
   },
